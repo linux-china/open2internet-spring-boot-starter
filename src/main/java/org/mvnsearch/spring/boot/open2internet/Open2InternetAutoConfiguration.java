35009ce2291398759ff0724d34a9128c1a7aa9f8
linux_china <libing.chen@gmail.com>
2018-11-30 11:15:11 -0800

    Open2Internet auto configuration

---
+++ src/main/java/org/mvnsearch/spring/boot/open2internet/Open2InternetAutoConfiguration.java
@@ -0,0 +1,133 @@
+package org.mvnsearch.spring.boot.open2internet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.ByteBufferBackedInputStream;
+import io.rsocket.AbstractRSocket;
+import io.rsocket.Payload;
+import io.rsocket.RSocketFactory;
+import io.rsocket.uri.UriTransportRegistry;
+import io.rsocket.util.DefaultPayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Open2Internet auto configuration
+ *
+ * @author linux_china
+ */
+@Configuration
+@EnableConfigurationProperties(Open2InternetProperties.class)
+public class Open2InternetAutoConfiguration implements ApplicationListener<WebServerInitializedEvent> {
+    private static String hint = "open2internet by @linux_china\n" +
+            "\n" +
+            "Connected Status              online\n" +
+            "Management Token              %s\n" +
+            "Internet Web Interface        %s\n" +
+            "Local Web Interface           %s\n" +
+            "Forwarding Rule               %s -> %s\n";
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private Open2InternetProperties properties;
+    private WebClient webClient;
+    private String localWebUri = "http://127.0.0.1:8080";
+
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int localListenPort = event.getWebServer().getPort();
+        this.localWebUri = "http://127.0.0.1:" + localListenPort;
+        this.webClient = WebClient.create(localWebUri);
+        Open2InternetAuthentication authentication = new Open2InternetAuthentication(properties.getAccessToken(), properties.getCustomDomain());
+        RSocketFactory
+                .connect()
+                .setupPayload(DefaultPayload.create(authentication.toString()))
+                .acceptor(rsocketPeer -> new AbstractRSocket() {
+                    @Override
+                    public Mono<Payload> requestResponse(Payload payload) {
+                        try {
+                            HttpRequest httpRequest = objectMapper.readValue(new ByteBufferBackedInputStream(payload.getData()), HttpRequest.class);
+                            return getHttpResponse(httpRequest).map(httpResponse -> DefaultPayload.create(toJson(httpResponse)));
+                        } catch (Exception e) {
+                            return Mono.just(DefaultPayload.create(toJsonException(e)));
+                        }
+                    }
+
+                    @SuppressWarnings("unchecked")
+                    @Override
+                    public Mono<Void> fireAndForget(Payload payload) { //todo please use metadataPush instead later
+                        try {
+                            Map<String, Object> info = objectMapper.readValue(new ByteBufferBackedInputStream(payload.getData()), HashMap.class);
+                            if ("app.exposed".equals(info.get("eventType"))) {
+                                System.out.println(String.format(hint, info.get("token"), info.get("uri"), localWebUri, info.get("uri"), localWebUri));
+                            }
+                        } catch (Exception ignore) {
+
+                        }
+                        return Mono.empty();
+                    }
+                })
+                .transport(UriTransportRegistry.clientForUri(properties.getUri()))
+                .start()
+                .block();
+    }
+
+    public Mono<HttpResponse> getHttpResponse(HttpRequest httpRequest) {
+        WebClient.RequestBodySpec webRequest = webClient.method(HttpMethod.valueOf(httpRequest.getMethod()))
+                .uri(httpRequest.getRequestUri())
+                .headers(httpHeaders -> {
+                    for (Map.Entry<String, String> entry : httpRequest.getHeaders().entrySet()) {
+                        httpHeaders.add(entry.getKey(), entry.getValue());
+                    }
+                });
+        if (httpRequest.getBody() != null) {
+            webRequest.body(Mono.just(new ByteArrayResource(httpRequest.getBody())), ByteArrayResource.class);
+        }
+        Mono<ClientResponse> clientResponseMono = webRequest.exchange();
+        return clientResponseMono
+                .map(clientResponse -> {
+                    HttpResponse httpResponse = new HttpResponse();
+                    httpResponse.setStatus(clientResponse.rawStatusCode());
+                    HttpHeaders httpHeaders = clientResponse.headers().asHttpHeaders();
+                    for (String name : httpHeaders.keySet()) {
+                        httpResponse.addHeader(name, httpHeaders.getFirst(name));
+                    }
+                    return httpResponse;
+                })
+                .zipWith(clientResponseMono.flatMap(clientResponse -> clientResponse.bodyToMono(ByteArrayResource.class)), (httpResponse, byteArrayResource) -> {
+                    if (byteArrayResource != null) {
+                        httpResponse.setBody(byteArrayResource.getByteArray());
+                    }
+                    return httpResponse;
+                });
+    }
+
+    public String toJson(HttpResponse httpResponse) {
+        try {
+            return objectMapper.writeValueAsString(httpResponse);
+        } catch (Exception e) {
+            return "{}";
+        }
+    }
+
+    public String toJsonException(Exception e) {
+        HttpResponse httpResponse = new HttpResponse();
+        httpResponse.setStatus(500);
+        httpResponse.addHeader("Content-Type", "text/plain;charset=UTF-8");
+        httpResponse.setBody(e.getMessage().getBytes());
+        return toJson(httpResponse);
+    }
+
+}
