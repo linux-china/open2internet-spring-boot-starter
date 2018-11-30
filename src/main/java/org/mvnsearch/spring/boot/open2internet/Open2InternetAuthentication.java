613133eeb39e60761be1bfe0ddb5cbfd773b507e
linux_china <libing.chen@gmail.com>
2018-11-30 11:18:46 -0800

    open2internet authentication

---
+++ src/main/java/org/mvnsearch/spring/boot/open2internet/Open2InternetAuthentication.java
@@ -0,0 +1,47 @@
+package org.mvnsearch.spring.boot.open2internet;
+
+/**
+ * open2internet authentication
+ *
+ * @author linux_china
+ */
+public class Open2InternetAuthentication {
+    private String token;
+    private String domain;
+
+    public Open2InternetAuthentication(String token) {
+        this.token = token;
+    }
+
+    public Open2InternetAuthentication(String token, String domain) {
+        this.token = token;
+        this.domain = domain;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    @Override
+    public String toString() {
+        if (token == null || domain == null || token.isEmpty() || domain.isEmpty()) {
+            return "{}";
+        }
+        return "{" +
+                "\"token\":\"" + token + '\"' +
+                ", \"domain\":\"" + domain + '\"' +
+                '}';
+    }
+}
