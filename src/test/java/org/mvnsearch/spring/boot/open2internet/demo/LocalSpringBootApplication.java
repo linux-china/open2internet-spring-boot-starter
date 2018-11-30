271f0652be41c9a88dfa9dc4af3f100ad54cc0ae
linux_china <libing.chen@gmail.com>
2018-11-30 11:16:57 -0800

    local Spring Boot demo application

---
+++ src/test/java/org/mvnsearch/spring/boot/open2internet/demo/LocalSpringBootApplication.java
@@ -0,0 +1,18 @@
+package org.mvnsearch.spring.boot.open2internet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * local Spring Boot demo application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class LocalSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalSpringBootApplication.class, args);
+    }
+
+}
