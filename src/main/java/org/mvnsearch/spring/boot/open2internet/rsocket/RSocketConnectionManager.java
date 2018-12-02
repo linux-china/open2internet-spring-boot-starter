157354ba4ba3ec9b01ea4b59154349c809846494
linux_china <libing.chen@gmail.com>
2018-12-01 21:01:59 -0800

    RSocket connection manager

---
+++ src/main/java/org/mvnsearch/spring/boot/open2internet/rsocket/RSocketConnectionManager.java
@@ -0,0 +1,15 @@
+package org.mvnsearch.spring.boot.open2internet.rsocket;
+
+/**
+ * RSocket connection manager
+ *
+ * @author linux_china
+ */
+public interface RSocketConnectionManager {
+
+    void connect();
+
+    void disConnect();
+
+    boolean isConnected();
+}
