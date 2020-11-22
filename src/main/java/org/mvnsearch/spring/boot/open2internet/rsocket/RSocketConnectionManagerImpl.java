11ee07e99ba990b148d955274457ba79f607305d
linux_china <libing.chen@gmail.com>
2020-11-22 11:23:15 -0800

    add multi upstream uri support

--- src/main/java/org/mvnsearch/spring/boot/open2internet/rsocket/RSocketConnectionManagerImpl.java
+++ src/main/java/org/mvnsearch/spring/boot/open2internet/rsocket/RSocketConnectionManagerImpl.java
@@ -1,7 +1,6 @@
 package org.mvnsearch.spring.boot.open2internet.rsocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.rsocket.AbstractRSocket;
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
 import io.rsocket.core.RSocketConnector;
@@ -15,7 +14,9 @@ import reactor.core.publisher.Mono;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,99 +30,107 @@ public class RSocketConnectionManagerImpl extends JsonSupport implements RSocket
     /**
      * authentication to upstream rsocket
      */
-    private Open2InternetAuthentication authentication;
+    private final Open2InternetAuthentication authentication;
     /**
      * upstream rsocket uri
      */
-    private String upstreamRsocketUri;
+    private final String[] upstreamRsocketUris;
     /**
      * RSocket connection
      */
-    private RSocket rsocket;
+    private final List<RSocket> rsockets = new ArrayList<>();
     /**
      * connect info
      */
-    private ConnectInfo connectInfo;
+    private final List<ConnectInfo> connectInfoList = new ArrayList<>();
 
-    private LocalHttpServiceClient localHttpServiceClient;
+    private final LocalHttpServiceClient localHttpServiceClient;
 
-    public RSocketConnectionManagerImpl(ObjectMapper objectMapper, String upstreamRsocketUri, Open2InternetAuthentication authentication, LocalHttpServiceClient localHttpServiceClient) {
+    public RSocketConnectionManagerImpl(ObjectMapper objectMapper, String[] upstreamRsocketUris, Open2InternetAuthentication authentication, LocalHttpServiceClient localHttpServiceClient) {
         super(objectMapper);
-        this.upstreamRsocketUri = upstreamRsocketUri;
+        this.upstreamRsocketUris = upstreamRsocketUris;
         this.authentication = authentication;
         this.localHttpServiceClient = localHttpServiceClient;
     }
 
     public void connect() {
-        if (this.rsocket != null && rsocket.availability() != 0.0) {
+        if (!this.rsockets.isEmpty()) {
             return;
         }
-        URI serverUri = URI.create(upstreamRsocketUri);
-        // connect to internet exposed service gateway
-        rsocket = RSocketConnector
-                .create()
-                .setupPayload(DefaultPayload.create(authentication.toString()))
-                .acceptor((connectionSetupPayload, rSocket) -> Mono.just(new AbstractRSocket() {
-                    @Override
-                    public Mono<Payload> requestResponse(Payload payload) {
-                        try {
-                            HttpRequest httpRequest = readValue(payload.getData(), HttpRequest.class);
-                            return localHttpServiceClient.getHttpResponse(httpRequest).map(httpResponse -> DefaultPayload.create(toJson(httpResponse)));
-                        } catch (Exception e) {
-                            return Mono.just(DefaultPayload.create(toJson(e)));
-                        }
-                    }
-
-                    @Override
-                    public Mono<Void> fireAndForget(Payload payload) {
-                        displayInfo(payload.getData());
-                        return Mono.empty();
-                    }
-
-                    @Override
-                    public Mono<Void> metadataPush(Payload payload) {
-                        displayInfo(payload.getMetadata());
-                        return Mono.empty();
-                    }
-
-                    private void displayInfo(ByteBuffer jsonData) {
-                        try {
-                            @SuppressWarnings("unchecked")
-                            Map<String, Object> info = readValue(jsonData, HashMap.class);
-                            if ("app.exposed".equals(info.get("eventType"))) {
-                                connectInfo = new ConnectInfo();
-                                connectInfo.setInternetUri((String) info.get("uri"));
-                                connectInfo.setLocalBaseWebUri(localHttpServiceClient.getLocalBaseWebUri());
-                                connectInfo.setAccessToken((String) info.get("token"));
-                                connectInfo.setQrCodeUri("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + URLEncoder.encode(connectInfo.getInternetUri(), "utf-8"));
-                                outputAsGreen(connectInfo.hint());
+        for (String upstreamRsocketUri : upstreamRsocketUris) {
+            URI serverUri = URI.create(upstreamRsocketUri);
+            // connect to internet exposed service gateway
+            RSocket rsocket = RSocketConnector
+                    .create()
+                    .setupPayload(DefaultPayload.create(authentication.toString()))
+                    .acceptor((connectionSetupPayload, rSocket) -> Mono.just(new RSocket() {
+                        @Override
+                        public Mono<Payload> requestResponse(Payload payload) {
+                            try {
+                                HttpRequest httpRequest = readValue(payload.getData(), HttpRequest.class);
+                                return localHttpServiceClient.getHttpResponse(httpRequest).map(httpResponse -> DefaultPayload.create(toJson(httpResponse)));
+                            } catch (Exception e) {
+                                return Mono.just(DefaultPayload.create(toJson(e)));
                             }
-                        } catch (Exception ignore) {
-
                         }
-                    }
-                }))
-                .connect(TcpClientTransport.create(serverUri.getHost(), serverUri.getPort()))
-                .block();
+
+                        @Override
+                        public Mono<Void> fireAndForget(Payload payload) {
+                            displayInfo(payload.getData());
+                            return Mono.empty();
+                        }
+
+                        @Override
+                        public Mono<Void> metadataPush(Payload payload) {
+                            displayInfo(payload.getMetadata());
+                            return Mono.empty();
+                        }
+
+                        private void displayInfo(ByteBuffer jsonData) {
+                            try {
+                                @SuppressWarnings("unchecked")
+                                Map<String, Object> info = readValue(jsonData, HashMap.class);
+                                if ("app.exposed".equals(info.get("eventType"))) {
+                                    ConnectInfo connectInfo = new ConnectInfo();
+                                    connectInfo.setInternetUri((String) info.get("uri"));
+                                    connectInfo.setLocalBaseWebUri(localHttpServiceClient.getLocalBaseWebUri());
+                                    connectInfo.setAccessToken((String) info.get("token"));
+                                    connectInfo.setQrCodeUri("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + URLEncoder.encode(connectInfo.getInternetUri(), "utf-8"));
+                                    outputAsGreen(connectInfo.hint());
+                                    connectInfoList.add(connectInfo);
+                                }
+                            } catch (Exception ignore) {
+
+                            }
+                        }
+                    }))
+                    .connect(TcpClientTransport.create(serverUri.getHost(), serverUri.getPort()))
+                    .block();
+            //todo deal with connection closed from upstream
+            this.rsockets.add(rsocket);
+        }
     }
 
     public void disConnect() {
-        if (rsocket != null) {
-            rsocket.dispose();
+        for (RSocket rsocket : rsockets) {
+            try {
+                rsocket.dispose();
+            } catch (Exception ignore) {
+
+            }
         }
-        rsocket = null;
-        connectInfo = null;
+        connectInfoList.clear();
         outputAsGreen("Disconnected from Open2Internet.");
     }
 
     @Override
     public boolean isConnected() {
-        return rsocket != null && rsocket.availability() > 0;
+        return !rsockets.isEmpty();
     }
 
     @Override
-    public ConnectInfo getConnectInfo() {
-        return this.connectInfo;
+    public List<ConnectInfo> getConnectInfo() {
+        return this.connectInfoList;
     }
 
     private void outputAsGreen(String text) {
